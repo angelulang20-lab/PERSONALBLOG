89b6def46ad66cf8c65fa35388a21174918b0213
Catlyn Alomia <catlynalomia@gmail.com>
2025-11-12 21:58:10 +0800

    excluded author in admin panel

--- PersonalBlog/admin.py
+++ PersonalBlog/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    exclude = ('author',)
+    
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
